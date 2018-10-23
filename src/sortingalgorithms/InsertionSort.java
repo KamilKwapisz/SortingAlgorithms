89899a23f5bda4e4f6b5ad76e37eebb7ffc6c41a
KamilKwapisz <kamiltm1998@gmail.com>
2018-10-23 10:56:58 +0200

    insort appearance improved

--- src/sortingalgorithms/InsertionSort.java
+++ src/sortingalgorithms/InsertionSort.java
@@ -3,13 +3,13 @@ package sortingalgorithms;
 public class InsertionSort {
     public double[] sort(double[] vector){
         for (int i = 1; i < vector.length; i++){
-            double tmp = vector[i];
+            double elementToInsert = vector[i];
             int j = i - 1;
-            while (j >= 0 && vector[j] > tmp){
+            while (j >= 0 && vector[j] > elementToInsert){
                 vector[j+1] = vector[j];
                 j--;
             }
-            vector[j+1] = tmp;
+            vector[j+1] = elementToInsert;
         }
         return vector;
     }
